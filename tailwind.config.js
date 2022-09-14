f4ab9956882e39db20ff2a0392b224c932276547
saezuri510 <saezuru510@gmail.com>
2022-09-14 22:53:48 +0900

    portfolio-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,9 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {
-            colors: {
-                primary: {
-                    100: "#ebf8ff",
-                    300: "#90cdf4",
-                    500: "#4299e1",
-                },
-            },
+        extend: {},
+        fontFamily: {
+            poppins: ["Poppins", "sans-serif"],
         },
     },
     plugins: [],
